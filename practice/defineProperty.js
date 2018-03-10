add549011c149872de85d2471d27a991ba093187
LEE <wiolem@hotmail.com>
2018-03-10 18:12:52 +0800

    :art: update defineProperty.js

--- practice/defineProperty.js
+++ practice/defineProperty.js
@@ -1,5 +1,5 @@
 var o = {}
-var temp = 0
+var temp
 Object.defineProperty(o, "a", {
   get : function(){
     console.log('a = ' + (temp + 1))
@@ -11,5 +11,6 @@ Object.defineProperty(o, "a", {
   enumerable : true,
   configurable : true
 })
+o.a = 0
 if(o.a === 1 && o.a === 2 && o.a === 3 && o.a === 4) console.log('success')
 // o 换成 window 可在浏览器环境下实现
